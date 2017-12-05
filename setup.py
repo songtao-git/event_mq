993eeadc291788b94f82da87d026db9a804ba313
SongTao <songtao@klicen.com>
2017-12-05 16:14:19 +0800

    rm readme.md

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 # -*- coding:utf-8 -*-
-import os
-import sys
 from distutils.core import setup
-
-
-def read(fname):
-    """
-    define 'read' func to read long_description from 'README.txt'
-    """
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='event-mq',
-    version='0.1',
+    version='0.2',
     packages=['event_mq'],
     description='A Event Queue for communication between progress.',
-    long_description=read('README.md'),
     url='https://github.com/songtao-git/event_mq',
     author='songtao',
     author_email='975765671@qq.com',
